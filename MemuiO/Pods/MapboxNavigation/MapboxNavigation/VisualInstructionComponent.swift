f888acf0356352cc3dbc1b70ee41945e22a3d4c3
Tejaswini N <nteju37@gmail.com>
2020-12-23 19:16:31 +0530

    add

---
+++ MemuiO/Pods/MapboxNavigation/MapboxNavigation/VisualInstructionComponent.swift
@@ -0,0 +1,22 @@
+import UIKit
+import MapboxDirections
+
+extension VisualInstruction.Component {
+    static let scale = UIScreen.main.scale
+    
+    var cacheKey: String? {
+        switch self {
+        case let .exit(representation), let .exitCode(representation):
+            let exitCode = representation.text
+            return "exit-" + exitCode + "-\(VisualInstruction.Component.scale)"
+        case let .image(imageRepresentation, alternativeText):
+            guard let imageURL = imageRepresentation.imageBaseURL else {
+                return "generic-" + alternativeText.text
+            }
+            
+            return "\(imageURL.absoluteString)-\(VisualInstruction.Component.scale)"
+        case .text, .delimiter, .lane:
+            return nil
+        }
+    }
+}
